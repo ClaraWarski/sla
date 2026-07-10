8ebd717a47c8745c6f31bbd3e025520a7bbe2b8b
Clara Warski <outlierjose88@gmail.com>
2026-07-10 16:41:01 -0300

    Delete photos-data.js

--- photos-data.js
+++
@@ -1,8 +0,0 @@
-// ===== FOTOS DO CASAL (carrossel 3D) =====
-// Cole aqui as URLs das fotos (do GitHub, ou de qualquer link direto de imagem).
-// Pode ter quantas quiser -- o carrossel se ajusta sozinho.
-const WC_PHOTOS = [
-  // 'foto1.jpg',
-  // 'foto2.jpg',
-  // 'foto3.jpg',
-];
